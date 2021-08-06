2e002b0769f3b643c32126d00f9483943cc9ff67
Juril Digamon <digamonjurild@gmail.com>
2021-08-06 08:32:54 +0800

    Add meta line for robots

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: 'Meta description' }
+			{ hid: 'description', name: 'description', content: '' },
+			{ hid: 'robots', name: 'robots', content: 'index, follow' },
     ],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png?v=1.0' }
